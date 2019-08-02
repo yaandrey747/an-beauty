d0e72a5cb326c92e54b85101ceb102a6559f2f7a
Yaandrey <yaandrey747@mail.ru>
2019-08-02 16:06:52 +0400

    no message

--- ANBeauty/send.php
+++ ANBeauty/send.php
@@ -9,10 +9,10 @@ if(isset($_POST['submit'])){
 /* Устанавливаем e-mail Кому и от Кого будут приходить письма */    
 	$to = "anbeauty@an-beauty64.ru"; // Здесь нужно написать e-mail, куда будут приходить письма	
     $from = "anbeauty@an-beauty64.ru"; // Здесь нужно написать e-mail, от кого будут приходить письма, например no-reply@epicblog.net
-}
+
 /* Указываем переменные, в которые будет записываться информация с формы */
 	$name = $_POST['message-name'];
-	$first_name = $_POST['message-firstname']
+	$first_name = $_POST['message-firstname'];
 	$email = $_POST['message-email'];
 	$phone = $_POST['phone'];
 	$message = $_POST['message'];
@@ -36,10 +36,10 @@ E-mail: $email
 $headers = "From: $from \r\n";
 	
 /* Отправка сообщения, с помощью функции mail() */
-    @mail($to, $subject, $mail_to_myemail, $headers . 'Content-type: text/plain; charset=utf-8');
+    mail($to, $subject, $mail_to_myemail, $headers . 'Content-type: text/plain; charset=utf-8');
     echo "Сообщение отправлено. Спасибо Вам " . $name . ", мы скоро свяжемся с Вами.";
 	echo "<br /><br /><a href='https://an-beauty64.ru'>Вернуться на сайт.</a>";
-
+}
 ?>
 <!--Переадресация на главную страницу сайта, через 3 секунды-->
 <script language="JavaScript" type="text/javascript">
